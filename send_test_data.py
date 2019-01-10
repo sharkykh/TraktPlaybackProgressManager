9dee42dedf2c68e68622d7df68839c8b8fd3ff83
sharkykh <sharkykh@gmail.com>
2019-01-10 21:22:25 +0200

    Add random progress to `send_test_data.py`

--- send_test_data.py
+++ send_test_data.py
@@ -3,20 +3,26 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import random
+
 from tppm import Application
 
 from trakt import Trakt
 
 
+def gen_progress():
+    return random.uniform(0.0, 100.0)
+
+
 def send_test_data():
-    Trakt['scrobble'].pause(movie={'ids': {'tmdb': 118340}})
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5761493}})
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5951173}})
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5950264}})
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5966778}})
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5966780}})
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5940102}})
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 6636942}})
+    Trakt['scrobble'].pause(movie={'ids': {'tmdb': 118340}}, progress=gen_progress())
+    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5761493}}, progress=gen_progress())
+    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5951173}}, progress=gen_progress())
+    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5950264}}, progress=gen_progress())
+    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5966778}}, progress=gen_progress())
+    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5966780}}, progress=gen_progress())
+    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5940102}}, progress=gen_progress())
+    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 6636942}}, progress=gen_progress())
 
     print('Test data sent.')
 
