3332208d6ee51d6beeb2e7e141617872bb1d4cd8
sharkykh <sharkykh@gmail.com>
2019-01-17 09:20:27 +0200

    Refactor `send_test_data.py`

--- send_test_data.py
+++ send_test_data.py
@@ -4,10 +4,25 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import random
+from time import sleep
 
 from tppm import Application
 
 from trakt import Trakt
+
+TEST_DATA = {
+    'movie': [
+        {'tmdb': 404368},  # Ralph Breaks the Internet (2018)
+    ],
+    'episode': [
+        {'tvdb': 5951173},  # Tá no Ar: A TV na TV: 4x04 Episode 4
+        {'tvdb': 5950264},  # Democracy Now!: 2017x32 Tuesday, February 14, 2017
+        {'tvdb': 5966778},  # All In with Chris Hayes: 2017x32 February 14, 2017
+        {'tvdb': 5966780},  # All In with Chris Hayes: 2017x33 February 15, 2017
+        {'tvdb': 5940102},  # Chicago Fire: 5x13 Trading in Scuttlebutt
+        {'tvdb': 6636942},  # Westworld: 2x03 Virtù e Fortuna
+    ]
+}
 
 
 def gen_progress():
@@ -15,14 +30,21 @@ def gen_progress():
 
 
 def send_test_data():
-    Trakt['scrobble'].pause(movie={'ids': {'tmdb': 404368}}, progress=gen_progress())
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5761493}}, progress=gen_progress())
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5951173}}, progress=gen_progress())
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5950264}}, progress=gen_progress())
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5966778}}, progress=gen_progress())
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5966780}}, progress=gen_progress())
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 5940102}}, progress=gen_progress())
-    Trakt['scrobble'].pause(episode={'ids': {'tvdb': 6636942}}, progress=gen_progress())
+    index = 1
+    total = len(TEST_DATA['movie']) + len(TEST_DATA['episode'])
+    for category, items in TEST_DATA.items():
+        for item in items:
+            params = {
+                category: {
+                    'ids': item
+                },
+                'progress': gen_progress()
+            }
+            print('Sending [%d/%d]: %s' % (index, total, item))
+            Trakt['scrobble'].pause(**params)
+            index += 1
+
+            sleep(0.1)
 
     print('Test data sent.')
 
